a05fe9c08c424ce3d42a4a16fba1fd1aff01377c
spiderkam <107589340+spiderkam@users.noreply.github.com>
2022-06-15 18:49:43 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function newpizza(){
+    document.getElementById("imgs").src="https://www.simplyrecipes.com/thmb/8caxM88NgxZjz-T2aeRW3xjhzBg=/2000x1125/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__09__easy-pepperoni-pizza-lead-3-8f256746d649404baa36a44d271329bc.jpg";
+}
+function pizza(){
+    document.getElementById("imgs").src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F19%2F2014%2F07%2F10%2Fpepperoni-pizza-ck-x.jpg&q=60";
+}
